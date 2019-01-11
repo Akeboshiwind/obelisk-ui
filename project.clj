d4fe746d68bcd5fab7ef41c546998ca3bf6f4695
Oliver Marshall <oliver.marshall@thetrainline.com>
2019-01-11 23:38:21 +0000

    Fix project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject obelisk-ui "0.1.0"
-  :description "A clojure wrapper for the api calls that the [obelisk](https://obelisk."
+  :description "A clojure wrapper for the api calls that the [obelisk](https://obelisk.tech/) miner ui makes."
   :url "https://github.com/akeboshiwind/obelisk-ui"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
